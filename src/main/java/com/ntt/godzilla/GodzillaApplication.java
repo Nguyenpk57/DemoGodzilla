1b19b24f9203014e74a7cae3f78f78cbb15ed316
nguyenkhoipham1410 <nguyenkhoipham1410@gmail.com>
2023-01-11 17:42:51 +0700

    init project

---
+++ src/main/java/com/ntt/godzilla/GodzillaApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.godzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodzillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntt/godzilla/GodzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.godzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
