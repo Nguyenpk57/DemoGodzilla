37f8d8ac6b13bc003a28747a0dfcf503da339ebb
BachVu <bachvu236@gmail.com>
2023-01-31 15:18:32 +0700

    Create Image entity

--- src/main/java/com/ntt/godzilla/entity/Image.java
+++ src/main/java/com/ntt/godzilla/entity/Image.java
@@ -7,36 +7,38 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
-@Entity
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "category")
-public class Category implements Serializable {
+@Getter
+@Setter
+public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
-    private Long categoryId;
-    @Column(name = "category_name")
-    private String categoryName;
-    @Column(name = "description")
-    private String description;
-    @Column(name = "slug")
-    private String slug;
-    @Column(name = "status")
-    private Integer status;
+    private Long image_id ;
+    @Column(name = "product_id")
+    private Long  product_id ;
+    @Column
+    private String image_type;
+    private Integer image_size;
+    private String image_name ;
+    private String image_path ;
+
     @Column(name = "create_user")
     private String createUser;
+
     @Column(name = "create_time", nullable = false, updatable = false)
     @CreationTimestamp
     private Date createTime;
+
     @Column(name = "update_user")
     private String updateUser;
+
     @Column(name = "update_time")
     @UpdateTimestamp
     private Date updateTime;
